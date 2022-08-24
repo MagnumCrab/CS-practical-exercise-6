f99ad976cf54c0cf5d6b752b6012dc5a56ab998c
Alexander_Napreenko <sasha-napreenko@yandex.ru>
2022-08-24 18:48:52 +0300

    add exercise_6.1

---
+++ exercise_6.1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write($"Введите количество чисел М: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] ArrayNumbers = new int[M];
+
+void EnterNumbers(int M)
+{
+for (int i = 0; i < M; i++)
+  {
+    Console.Write($"Введите {i+1} число: ");
+    ArrayNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+int Comparison(int[] ArrayNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < ArrayNumbers.Length; i++)
+  {
+    if(ArrayNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+EnterNumbers(M);
+Console.WriteLine($"Введено чисел больше 0: {Comparison(ArrayNumbers)} ");
